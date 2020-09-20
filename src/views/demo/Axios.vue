9a57e2041360c5efb748decef016a5c475e96618
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-20 11:05:42 +0800

    axios

---
+++ src/views/demo/Axios.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="axios">
+    <h2>Axios</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Axios",
+  created() {
+    this.$axios.post('/mock/demo/axios',{
+      t: new Date().getTime()
+    }).then((resp) => {
+      console.log(resp)
+    }).catch((resp) => {
+      console.error(resp)
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+<style scoped>
+
+</style>
