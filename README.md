593e877e38a18d0ca650f96c6fc818dff4fba11a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-21 11:17:27 +0800

    vant

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 ~~~
 npm install -D sass
 ~~~
+
+## [vant next](https://vant-contrib.gitee.io/vant/next/#/zh-CN/)
+~~~
+npm i vant@next
+~~~
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import Vant from 'vant'
 
 import App from './App.vue'
 
@@ -6,6 +7,7 @@ import router from './router'
 import store from './store'
 import axios from './axios'
 
+import 'vant/lib/index.css'
 import './scss/style.scss'
 import './index.css'
 
@@ -13,6 +15,7 @@ const app = createApp(App)
 
 app.use(store)
 app.use(router)
+app.use(Vant)
 
 app.mount('#app')
 
